b75308b7c82790190dcd3631c280f8b0f0411f3e
Tim <timturan@web.de>
2019-03-11 08:52:39 +0100

    Stack and Numpy Docstring konvention

--- stack.py
+++ stack.py
@@ -13,6 +13,16 @@ class Layer:
         self.flip = flip
         self.angle = angle
 
+    def flip(self):
+        self.flip = True
+        return
+
+    def mirror(self):
+        self.mirror = True
+
+    def rotate(self, angle):
+        self.angle = angle
+
 
 
 class MetaLayer(Layer):
@@ -40,21 +50,31 @@ class NonMetaLayer(Layer):
 
 class Stack:
 
-    def __init__(self, layer_list):
+    def __init__(self, layer_list, cladding, substrate):
         """
-        INPUT:
 
-        layer_list: list of Layer objects
+        Parameters
+        ----------
+        layer_list : list of Layer objects
+        cladding : float / vector
+                   The refrectiv Index of the material on top of the stack
+                   if the input is a single float n_i wavelength independent
+                   behavior will be assumed.
+        substrate : float / vectors
+                    The refractiv index of the material below the stack
+
         """
         self.layer_list = layer_list
-
+        self.cladding = cladding
+        self.substrate = substrate
 
     def create_propagator(self, nml):
         """
         Creates the propergator S-Matrix of a Non-Meta-Layers
 
-        INPUT:
-        nml: 1 NonMetaLayer object
+        Parameters
+        ----------
+        nml: NonMetaLayer object
         """
 
         #Height is a scalar
@@ -76,20 +96,28 @@ class Stack:
         """
         Creates the interface S-Matrix for the transmission between 2 Non-Meta-Layers
 
-        INPUT:
-        nml_1 , nml_2: 2 NonMetaLayer Objects
-        s_mat = np.zeros((nml_1.wav_vec_len,4,4)).astype(complex)
+        Parameters
+        ----------
+        nml_1 , nml_2:  NonMetaLayer Objects
         """
         #transmission and reflection in x and y directions
         T_x = 2*nml1.n_x/(nml_1.n_x + nml_2.n_x)
         T_y = 2*nml1.n_y/(nml_1.n_y + nml_2.n_y)
         R_x = (nml_1.n_x - nml_2.n_x)/(nml_1.n_x + nml_2.n_x)
         R_y = (nml_1.n_y - nml_2.n_y)/(nml_1.n_y + nml_2.n_y)
-        return np.array([[ T_x, 0  , R_x,    0],
-                         [ 0  , T_y,   0,  R_y],
-                         [-R_x, 0  , T_x,  0  ],
-                         [ 0  ,-R_y, 0   , T_y]
+        return np.array([[ T_x  , 0    , R_x,    0],
+                         [ 0    , T_y  ,   0,  R_y],
+                         [-1*R_x, 0    , T_x,  0  ],
+                         [ 0    ,-1*R_y, 0  , T_y ]
                         ])
+
+
+    def build(self):
+        """
+
+
+        """
+
 
 
 layer = NonMetaLayer(1/np.arange(1,5), [5.3, 4.3], np.arange(4), 2*np.arange(4))
