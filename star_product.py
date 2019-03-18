debd4bc008cf9c8ab7ac161b1b3925eb709bd0df
Tim <timturan@web.de>
2019-03-18 11:44:06 +0100

    numba experiments

--- star_product.py
+++ star_product.py
@@ -1,7 +1,10 @@
 import numpy as np
+from numba import jit
 
 
-def star_product_analyt(SIN_1,SIN_2):
+
+#@jit(nopython = True)
+def star_product_analyt(s_in1, s_in2, s_out, H, L):
     """
     Calculate Lifeng Li's starproduct for two S-matrices SIN_1 and SIN_2,
     such that S = S1 * S2. The starproduct between two arbitrary S-matrices
@@ -18,8 +21,7 @@ def star_product_analyt(SIN_1,SIN_2):
     -------
     s_out : HxLx4x4 numpy array
     """
-    height_vec_len = max(SIN_1.shape[0], SIN_2.shape[0])
-
+    """
     # S-matrix 1
     TF_1 = SIN_1[:,:,0:2,0:2]
     TB_1 = SIN_1[:,:,2:4,2:4]
@@ -33,7 +35,7 @@ def star_product_analyt(SIN_1,SIN_2):
     # number of wavelengths
     wav_vec_len = TF_1.shape[1]
     # declare output matrix
-    s_out = np.zeros((height_vec_len,wav_vec_len,4,4)).astype(complex)
+
 
     left_kernel = np.linalg.inv(np.eye(2) - RB_1 @ RF_2)
     right_kernel = np.linalg.inv(np.eye(2) - RF_2 @ RB_1)
@@ -47,6 +49,43 @@ def star_product_analyt(SIN_1,SIN_2):
     s_out[:,:,2:4,2:4] = TB
     s_out[:,:,2:4,0:2] = RF
     s_out[:,:,0:2,2:4] = RB
+    return s_out
+    """
+    for h in range(H):
+        for l in range(L):
+            TF_1 = s_in1[h,l,0:2,0:2]
+            TB_1 = s_in1[h,l,2:4,2:4]
+            RF_1 = s_in1[h,l,2:4,0:2]
+            RB_1 = s_in1[h,l,0:2,2:4]
+            # S-matrix 2
+            TF_2 = s_in2[h,l,0:2,0:2]
+            TB_2 = s_in2[h,l,2:4,2:4]
+            RF_2 = s_in2[h,l,2:4,0:2]
+            RB_2 = s_in2[h,l,0:2,2:4]
+
+            left_kernel = np.linalg.inv(np.eye(2) - RB_1 @ RF_2)
+            right_kernel = np.linalg.inv(np.eye(2) - RF_2 @ RB_1)
+
+            TF = TF_2 @ left_kernel @ TF_1
+            TB = TB_1 @ right_kernel @ TB_2
+            RF = RF_1 + TB_1 @ RF_2 @ left_kernel @ TF_1
+            RB = RB_2 + TF_2 @ RB_1 @ right_kernel @ TB_2
+            # Assemble the resulting s-matrix us_ing the elements from above
+            s_out[h,l,0:2,0:2] = TF
+            s_out[h,l,2:4,2:4] = TB
+            s_out[h,l,2:4,0:2] = RF
+            s_out[h,l,0:2,2:4] = RB
+
+            return
+
+def star_product(s_in1, s_in2):
+    """
+    Setup for numba JIT compilation
+    """
+    H = max(s_in1.shape[0], s_in2.shape[0])
+    L = s_in1.shape[1]
+    s_out = np.zeros((H,L,4,4)).astype(np.complex)
+    star_product_analyt(s_in1, s_in2, s_out, H, L)
     return s_out
 
 def star_product_geometric(SIN_1, SIN_2, order):
@@ -121,7 +160,7 @@ def star_product_cascaded(smat_list):
 
     smat = smat_list[0]
     for i in range(1, len(smat_list)):
-        smat = star_product_analyt(smat, smat_list[i])
+        smat = star_product(smat, smat_list[i])
 
     return smat
 
